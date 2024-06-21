69372c13e95615f25eea1778a8a9afcba620ce0f
Emanuele-web04 <emanueledipietro612@gmail.com>
2024-06-21 18:04:50 +0200

    Bit2Dec

---
+++ Bit2Dec/Bit2DecApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bit2DecApp.swift
+//  Bit2Dec
+//
+//  Created by Emanuele Di Pietro on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Bit2DecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
